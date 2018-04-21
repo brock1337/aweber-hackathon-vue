e0b7c6d2548d269d45dd6df3d93a5b334f5af32d
Robert Rock <brock@8thwonder.io>
2018-04-21 16:09:04 -0400

    Initial Commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>
+      Home
+    </h1>
+    
+    <div class="">
+      <button @click="enableNotifications" class="">
+        Enable Notifications
+      </button>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomePage",
+    methods: {
+      enableNotifications () {
+        if ('Notification' in window) {
+          Notification.requestPermission(result => {
+            console.log('Notification Permission: ', result);
+          
+            if (result !== 'granted') {
+              console.log('No Notification permission granted');
+            } else {
+              this.displayConfirmNotification();
+            }
+          });
+        } else {
+          console.log('Notification was not found in window');
+        }
+      },
+      displayConfirmNotification () {
+        let options = {
+          body: `Breaking News! Now you won't miss any!`,
+          icon: '../../static/img/icons/8w-globe-color-96x96.png',
+          dir: 'ltr',
+          lang: 'en-US'
+        };
+  
+        const notification = new Notification('Successfully subscribed!', options);
+        console.log(notification);
+  
+        notification.addEventListener('click', function(event) {
+          console.log('[Service Worker] Notification click Received.', event);
+
+          event.notification.close();
+
+          event.waitUntil(
+            self.clients.openWindow('https://developers.google.com/web/')
+          );
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
