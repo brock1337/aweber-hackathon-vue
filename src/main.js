c56158c05c9eb7fcf1a0ca719c4bb9b3f2abfcbe
Robert Rock <brock@8thwonder.io>
2018-04-21 19:20:44 -0400

    everything

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import '../build/css/mdb.css';
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
